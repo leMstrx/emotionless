8a621cfd15218f043c031a9a4e6848e03e04d7c1
leMstrx <moritz@koberling.net>
2024-11-23 23:49:51 +0100

    set up

--- main.py
+++ main.py
@@ -4,8 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ta.trend import SMAIndicator  # Example for technical analysis
 import backtrader as bt
+from config import *
 
-print("All libraries loaded successfully!")
+api = tradeapi.REST(ALPACA_API_KEY, ALPACA_SECRET_KEY, ALPACA_BASE_URL, api_version='v2')
+
+#Fetch account details
+
+try:
+    account = api.get_account()
+    print('Account ID', account.id)
+    print('Account Status', account.status)
+    print('Cash Balance', account.cash)
+    print('Equity', account.equity)
+except Exception as e:
+    print('Error fetching account details', str(e)) 
+
+
 
 
 
