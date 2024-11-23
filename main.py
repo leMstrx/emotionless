e61db65d59328f854bb2c65c622ffe09fcc619e3
leMstrx <moritz@koberling.net>
2024-11-23 11:48:27 +0100

    Env Setup

---
+++ main.py
@@ -0,0 +1,12 @@
+import alpaca_trade_api as tradeapi
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from ta.trend import SMAIndicator  # Example for technical analysis
+import backtrader as bt
+
+print("All libraries loaded successfully!")
+
+
+
+
