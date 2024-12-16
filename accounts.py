a2a1f9fd52371e85f5806cb2b5070dadb0772168
leMstrx <moritz@koberling.net>
2024-12-16 23:55:40 +0100

    solana fix

--- accounts.py
+++ accounts.py
@@ -1,6 +1,6 @@
 import json
-from solana.keypair import Keypair
 from pathlib import Path
+from solders.keypair import Keypair # type: ignore
 
 def load_keypair(keyfile_path: str = "local_keys/secret.json") -> Keypair:
     keypath = Path(keyfile_path)
@@ -10,7 +10,7 @@ def load_keypair(keyfile_path: str = "local_keys/secret.json") -> Keypair:
     with keypath.open("r") as keyfile:
         secret = json.load(keyfile)
 
-    #The secret should be a list of 64 integers
+    # The secret is typically a 64-byte array (64 integers).
+    # Convert it to bytes and use Keypair.from_bytes().
     secret_bytes = bytes(secret)
-    return Keypair.from_secret_key(secret_bytes)
-    
+    return Keypair.from_bytes(secret_bytes)
