e8d9984ab447b47c2eb49f85ba1b75be216ffcf3
leMstrx <moritz@koberling.net>
2024-12-18 00:49:20 +0100

    fresh start

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-# Putting all the non sensitive data here
+'''
+Contains all the important URLs and other Configurations
+'''
 
-#Free RPC endpoint (can later be replaced with PAID Tier)
-RPC_URL = "https://api.mainnet-beta.solana.com"
-
+RPC_ENDPOINT = "https://api.mainnet-beta.solana.com"
+KEYPAIR_PATH = "secret.json"
